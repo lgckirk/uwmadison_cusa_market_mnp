37593f480c1acf41f7313b02a5e6153cfbf653df
Mingxi Xu <mxu88@wisc.edu>
2018-01-03 00:33:42 -0600

    添加点击图片看大图功能

--- pages/2s/productDetail/wp.js
+++ pages/2s/productDetail/wp.js
@@ -87,4 +87,28 @@ Page({
       }
     })
   },
+
+
+
+ //图片点击事件
+ imgYu: function (event) {
+    var src = event.currentTarget.dataset.src;//获取data-src
+    var imgList = event.currentTarget.dataset.list;//获取data-list
+    if (src === "/images/usedItemMarket/buy/noPicture.png")
+    {
+      wx.showToast({
+        title: '无预览',
+        icon: 'loading',
+        duration: 600
+      })
+    }
+    
+    else{
+      //图片预览
+        wx.previewImage({
+          current: src, // 当前显示图片的http链接
+          urls: imgList // 需要预览的图片http链接列表
+      })
+    }
+  }
 })
