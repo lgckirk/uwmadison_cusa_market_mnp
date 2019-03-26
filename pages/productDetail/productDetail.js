37e75832d868fb35e309bf9870f1693bcefdfc5f
kirk <lgckirk@gmail.com>
2019-03-25 21:09:42 -0500

    Feature: let user share a product detail page.

--- pages/productDetail/productDetail.js
+++ pages/productDetail/productDetail.js
@@ -7,12 +7,72 @@ Page({
     array: util.getProductTypeArray(),
     index: 0
   },
+  onPullDownRefresh: function() {
+    wx.stopPullDownRefresh();
+  },
   onLoad: function(options){
-    this.setData({
-      'imgs': app.globalData.dispayedProduct.productImages,
-      'description': app.globalData.dispayedProduct.productDescription,
-      'type': app.globalData.dispayedProduct.productType
-    });
+    // id is required
+    if (!options["id"]) {
+      console.error("productDetail页面需要一个id作为argument");
+      return;
+    }
+
+    // product info could already be present
+    if (app.globalData["dispayedProduct"]
+      && app.globalData["dispayedProduct"]["productId"] == options["id"]) {
+        this.setData({
+          'imgs': app.globalData["dispayedProduct"]["productImages"],
+          'description': app.globalData["dispayedProduct"]["productDescription"],
+          'type': app.globalData["dispayedProduct"]["productType"]
+        });
+      }
+
+    // otherwise we need to load it
+    else {
+      wx.showLoading({
+        title: '正在加载...',
+      });
+      const timer = setTimeout(() => {
+        // a fail safe mechnism - we dont want to show it forever...
+        wx.hideLoading();
+      }, 5000);
+
+      util.getProductById(options["id"])
+      .then((product) => {
+        // stop the loading model
+        clearTimeout(timer);
+        wx.hideLoading();
+
+        // we got the product, now display
+        if (product) {
+          this.setData({
+            'imgs': product["productImages"],
+            'description': product["productDescription"],
+            'type': product["productType"]
+          });
+        }
+
+        // otherwise notify user
+        // TODO: use a custom image, right now the icon is a checkmark
+        else {
+          wx.showToast({
+            title: '好像出了点问题',
+            duration: 2000
+          });
+        }
+      });
+    }
+  },
+  onShareAppMessage(config) {
+    const ret = {
+      'title': app.globalData["dispayedProduct"].productName
+          + ': ' + app.globalData["dispayedProduct"].productDescription
+    };
+    const images = app.globalData["dispayedProduct"].productImages;
+    ret['imageUrl'] = images.length > 0 ?
+        images[0] : '/images/noPicture.png';
+
+    return ret;
   },
   imgErr: function(event){
       let buffer = {};
@@ -32,5 +92,10 @@ Page({
   },
   previewImages: function(){
     wx.previewImage({urls: this.data.imgs});
+  },
+  goHome: function() {
+    wx.switchTab({
+      url: '/pages/index/index',
+    });
   }
 })
